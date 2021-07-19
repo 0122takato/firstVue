1fc1ff178aed61cd607e1409548c8ade275d4576
0122takato <74225711+0122takato@users.noreply.github.com>
2021-07-19 22:09:57 +0900

    script.jsの記述完了

--- script.js
+++ script.js
@@ -3,3 +3,13 @@
  * 
  * モジュールを利用する
  */
+
+
+export function MyObj() {
+    return {
+        red:255, green:125, blue:0,
+        total:function() {
+            return this.red + this.green + this.blue;
+        }
+    };
+};
